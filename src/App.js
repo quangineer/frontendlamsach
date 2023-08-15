fb89400fef83b3ff9c4f57558e65c8e731b824aa
quangineer <quangineer@gmail.com>
2023-08-15 15:54:15 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CreateBook from "./components/createbook";
+import BookList from "./components/booklist";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import './App.css';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<BookList/>} />
+        <Route path="/createbook" element={<CreateBook/>}/>
+      </Routes>
+      </BrowserRouter>
+  );
+}
